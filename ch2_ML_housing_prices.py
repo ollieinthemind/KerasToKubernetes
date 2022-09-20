fe1263ad033e453f26446093cecf6030fa66949d
dennis.mcilhany <dennis.mcilhany@gmail.com>
2022-09-20 13:24:58 -0700

    hecko

--- ch2_ML_housing_prices.py
+++ ch2_ML_housing_prices.py
@@ -4,3 +4,11 @@ import pandas as pd
 features = pd.read_csv('data/house.price.csv')
 features.head(10)
 
+# We will use the K-Means algorithm
+from sklearn.cluster import KMeans
+# We will only consider 2 features and see if we get a pattern
+cluster_Xs = features[['Area', 'Locality']]
+# How many clusters we want to find
+NUM_CLUSTERS = 3
+# Build the K Means Clusters model
+model = KMeans(n_clusters=NUM_CLUSTERS)
